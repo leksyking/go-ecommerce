eb6477ec69947034a7388593d8f3814009478be8
Leksyking <gbemilekeogundipe@gmail.com>
2022-05-05 07:14:55 -0700

    define functions

--- database/databasesetup.go
+++ database/databasesetup.go
@@ -1,1 +1,15 @@
+package database
 
+import "go.mongodb.org/mongo-driver/mongo"
+
+func DBSet() *mongo.Client {
+
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
+
+func productData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
