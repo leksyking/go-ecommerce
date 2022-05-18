609c4dbab9231883a26c0fb3b8026c023a18351c
Leksyking <gbemilekeogundipe@gmail.com>
2022-05-18 08:49:09 -0700

    add products to cart function

--- database/cart.go
+++ database/cart.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/leksyking/go-ecommerce/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 var (
 	ErrCantFindProduct    = errors.New("Can't find the product")
@@ -12,8 +21,29 @@ var (
 	ErrCantBuyCartItem    = errors.New("Can't update the purchase")
 )
 
-func AddProductToCart() {
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	searchfromdb, err := prodCollection.Find(ctx, bson.M{"_id": productID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindProduct
+	}
+	var productCart []models.ProductUser
+	if err := searchfromdb.All(ctx, &productCart); err != nil {
+		log.Println(err)
+		return ErrCantDecodepPoducts
+	}
 
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "&push", Value: bson.D{primitive.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
 }
 
 func RemoveItemFromCart() {
